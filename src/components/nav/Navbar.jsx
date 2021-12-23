ce07ed562de35c21c02b70409e02dc03847e9aa9
dnh33 <3074491+dnh33@users.noreply.github.com>
2021-12-23 20:39:27 +0100

    show/hide navbar based on url

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,22 +1,41 @@
 import React from "react";
 import { Link } from "@gatsbyjs/reach-router";
+import Logo from "./Logo";
 
 export default function Navbar() {
+  const url = window.location.href;
+  console.log(url);
   return (
-    <nav className="flex items-center justify-around flex-wrap p-6 mx-auto sm:w-full md:w-11/12 lg:w-4/5 xl:w-entrypoint-nav h-28 mb-4 md:mb-8 lg:mb-59 drop-shadow-lg border-solid border-b-4 border-x-4 border-opacity-10 border-black md:rounded-b-entrypoint bg-gradient-to-r from-entry-pink to-entry-purple ">
+    <nav className="flex items-center justify-around flex-wrap p-6 mx-auto sm:w-full md:w-11/12 lg:w-4/5 xl:w-entrypoint-nav h-28 mb-4 md:mb-8 lg:mb-59 drop-shadow-lg border-solid border-b-4 border-x-4 border-opacity-10 border-black md:rounded-b-entrypoint bg-gradient-to-r from-entry-pink to-entry-purple">
+      <Logo />
       <div className="md:flex content-center items-center">
-        <Link
-          to="/"
-          className="text-xl py-4 px-6 text-white font-semibold hover:animate-pulse hover:from-pink-500 hover:to-yellow-500"
-        >
-          <span className="uppercase shadow">home</span>
-        </Link>
+        {url !== "http://localhost:3000/" ? (
+          <Link
+            to="/"
+            className="text-xl py-4 px-6 text-white font-semibold hover:animate-pulse hover:from-pink-500 hover:to-yellow-500"
+          >
+            <span className="uppercase shadow">home</span>
+          </Link>
+        ) : null}
         <Link
           to="/about"
           className="text-xl py-4 px-6 text-white font-semibold hover:animate-pulse hover:from-pink-500 hover:to-yellow-500"
         >
           <span className="uppercase shadow">about</span>
         </Link>
+        <Link
+          to="/videos"
+          className="text-xl py-4 px-6 text-white font-semibold hover:animate-pulse hover:from-pink-500 hover:to-yellow-500"
+        >
+          <span className="uppercase shadow">videos</span>
+        </Link>
+        <a
+          href="https://github.com/AnEntrypoint/universal-domains/wiki"
+          target="_blank"
+          className="text-xl py-4 px-6 text-white font-semibold hover:animate-pulse hover:from-pink-500 hover:to-yellow-500"
+        >
+          <span className="uppercase shadow">wiki</span>
+        </a>
         <Link
           to="/faq"
           className="text-xl py-4 px-6 text-white font-semibold hover:animate-pulse hover:from-pink-500 hover:to-yellow-500"
