0ef3b8b1667dccf7bc1b9aad2d1315d481465d9e
jb0gie <up620161@myport.ac.uk>
2021-12-29 17:32:57 +0000

    ◙

--- src/components/containers/Searchbox.jsx
+++ src/components/containers/Searchbox.jsx
@@ -24,7 +24,7 @@ export default function Searchbox() {
   return (
     <section className="fixed flex flex-col mx-auto left-0 right-0 md:rounded-80 h-searchbox-height sm:w-full md:w-4/5 lg:w-4/5 xl:w-searchbox-width conical-gradient border-solid border-b-4 border-x-4 border-opacity-10 border-black">
       <h1 className=".z-10 mt-entrypoint-title mx-auto text-4xl md:text-5xl lg:text-6xl xl:text-70 text-white box-shadow-custom">
-        <span className="uppercase font-bold">µniversal domains</span>
+        <span className="uppercase font-bold">◙universal  domains◙</span> 
       </h1>
       <h2 className=".z-20 mt-67 mx-auto text-xl md:text-2xl lg:text-3xl xl:text-4xl text-shadow text-white font-light">
         <span className="capitalize">the </span>easiest way to get a{" "}
